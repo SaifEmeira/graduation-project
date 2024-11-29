122fcbed457eeef67cb9f8a043cf329eeb5076f5
SaifEmeira <saifemeira@gmail.com>
2024-11-29 18:17:43 +0200

    section2Landing

--- src/components/Section2LandingPage.jsx
+++ src/components/Section2LandingPage.jsx
@@ -12,13 +12,13 @@ export default function Section2LandingPage() {
   return (
     <div className='bg-black overflow-hidden py-3 '>
       <div className='text-center'>
-    <h3 className='text-white fw-bold'>BEST DESTINATION</h3>
-    <p className='text-white'>Explore the enchanting landscapes of Egypt, from the breathtaking deserts to the stunning coastal shores.</p>
+    <h2 className='text-white fw-bold'  style={{ fontFamily: 'Bebas Neue' }}>BEST DESTINATION</h2>
+    <p className='text-white' >Explore the enchanting landscapes of Egypt, from the breathtaking deserts to the stunning coastal shores.</p>
       </div>
       <div className="row g-1 py-3 px-5">
         {/* Giza */}
         <div className="col-4">
-          <div className="card shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
+          <div className="card m-2 shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
             <img
               src={giza}
               alt="Giza"
@@ -26,12 +26,12 @@ export default function Section2LandingPage() {
               style={{ borderRadius: '15px', height: '400px' }}
             />
             <div className="card-img-overlay d-flex flex-column align-items-start">
-              <h5 className="card-title">Giza</h5>
+              <h5 className="card-title" style={{ fontFamily: 'Bebas Neue' }}>Giza</h5>
               <p>Lorem ipsum dolor sit amet consectetur.</p>
             </div>
           </div>
 
-          <div className="card my-2 shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
+          <div className="card m-2 shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
             <img
               src={aswan}
               alt="Aswan"
@@ -39,7 +39,7 @@ export default function Section2LandingPage() {
               style={{ borderRadius: "15px" ,height:"200px" }}
             />
             <div className="card-img-overlay d-flex align-items-start flex-column">
-              <h5 className="card-title">Aswan</h5>
+              <h5 className="card-title" style={{ fontFamily: 'Bebas Neue' }}>Aswan</h5>
               <p>Lorem ipsum dolor sit amet consectetur.</p>
 
             </div>
@@ -48,7 +48,7 @@ export default function Section2LandingPage() {
 
         {/* Luxor */}
         <div className="col-4 "style={{ height: '200px' }} >
-          <div className="card shadow-lg text-white border-0" style={{ borderRadius: "15px", }}>
+          <div className="card m-2 shadow-lg text-white border-0" style={{ borderRadius: "15px", }}>
             <img
               src={luxor}
               alt="Luxor"
@@ -56,12 +56,12 @@ export default function Section2LandingPage() {
               style={{ borderRadius: '15px', height: '200px' }}
             />
             <div className="card-img-overlay d-flex align-items-start flex-column">
-              <h5 className="card-title">Luxor</h5>
+              <h5 className="card-title" style={{ fontFamily: 'Bebas Neue' }}>Luxor</h5>
               <p>Lorem ipsum dolor sit amet consectetur.</p>
 
             </div>
           </div>
-          <div className="card my-2 shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
+          <div className="card m-2 shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
             <img
               src={hurghada}
               alt="Hurghada"
@@ -69,7 +69,7 @@ export default function Section2LandingPage() {
               style={{ borderRadius: '15px', height: '400px' }}
             />
             <div className="card-img-overlay d-flex align-items-start flex-column">
-              <h5 className="card-title">Hurghada</h5>
+              <h5 className="card-title" style={{ fontFamily: 'Bebas Neue' }}>Hurghada</h5>
               <p>Lorem ipsum dolor sit amet consectetur.</p>
 
             </div>
@@ -79,7 +79,7 @@ export default function Section2LandingPage() {
 
         {/* Sharm El-Sheikh */}
         <div className="col-4">
-          <div className="card shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
+          <div className="card m-2 shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
             <img
               src={sharm}
               alt="Sharm El-Sheikh"
@@ -87,13 +87,13 @@ export default function Section2LandingPage() {
               style={{ borderRadius: '15px', height: '400px' }}
             />
             <div className="card-img-overlay d-flex align-items-start flex-column">
-              <h5 className="card-title">Sharm El-Sheikh</h5>
+              <h5 className="card-title" style={{ fontFamily: 'Bebas Neue' }}>Sharm El-Sheikh</h5>
               <p>Lorem ipsum dolor sit amet consectetur.</p>
 
             </div>
           </div>
 
-          <div className="card my-2 shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
+          <div className="card m-2 shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
             <img
               src={alexandria}
               alt="Alexandria"
@@ -101,59 +101,14 @@ export default function Section2LandingPage() {
               style={{ borderRadius: "15px" , height: '200px' }}
             />
             <div className="card-img-overlay d-flex flex-column align-items-start">
-              <h5 className="card-title fw-bolder fs-2">Alexandria</h5>
+              <h5 className="card-title fw-bolder fs-2" style={{ fontFamily: 'Bebas Neue' }}>Alexandria</h5>
               <p>Lorem ipsum dolor sit amet consectetur.</p>
 
             </div>
           </div>
         </div>
 
-        {/* Aswan */}
-        {/* <div className="col-12 col-md-4">
-          <div className="card shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
-            <img
-              src={aswan}
-              alt="Aswan"
-              className="card-img-top"
-              style={{ borderRadius: "15px" }}
-            />
-            <div className="card-img-overlay d-flex align-items-end">
-              <h5 className="card-title">Aswan</h5>
-            </div>
-          </div>
-        </div> */}
-
-        {/* Hurghada */}
-        {/* <div className="col-12 col-md-4">
-          <div className="card shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
-            <img
-              src={hurghada}
-              alt="Hurghada"
-              className="card-img-top"
-              style={{ borderRadius: '15px', height: '400px' }}
-            />
-            <div className="card-img-overlay d-flex align-items-end">
-              <h5 className="card-title">Hurghada</h5>
-            </div>
-          </div>
-        </div> */}
-
-        
-
-        {/* Alexandria */}
-        {/* <div className="col-12 col-md-4">
-          <div className="card shadow-lg text-white border-0" style={{ borderRadius: "15px" }}>
-            <img
-              src={alexandria}
-              alt="Alexandria"
-              className="card-img-top"
-              style={{ borderRadius: "15px" }}
-            />
-            <div className="card-img-overlay d-flex align-items-end">
-              <h5 className="card-title">Alexandria</h5>
-            </div>
-          </div>
-        </div> */}
+       
       </div>
     </div>
 
